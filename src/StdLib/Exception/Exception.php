81f377562e02a53dc1924b8a0e30a86a302fdebf
Michael Hegenbarth <mnh@mn-hegenbarth.de>
2017-02-06 16:12:59 +0100

    add default Exception

---
+++ src/StdLib/Exception/Exception.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace MNHcC\MinimalusLayoutilus\StdLib\Exception;
+
+use \Exception as BaseException;
+
+/**
+ * 
+ * @todo add description to Exception
+ * @author Michael Hegenbarth <mnh@mn-hegenbarth.de>
+ */
+class Exception extends BaseException implements ExceptionInterface {
+    //put your code here
+}
